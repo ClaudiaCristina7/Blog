a28984255f957cf52c6695551913744d3df6f23c
ClaudiaCristina7 <Claudiac.cristina1986@gmail.com>
2026-07-06 22:25:05 +0300

    add vercel config

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,7 +1,7 @@
 import EditorPick from "../../components/editor-pick/editor-pick";
 import HeroSection from "../../components/hero-section/hero-section";
 import Populartopics from "../../components/popular-topics/popular-topics";
-import ThirdContentBlock from "../../components/third-page/third-content-block";
+import ThirdContentBlock from "../../components/topics-header/third-page/third-content-block";
 
 export default function Homepage() {
   return (
